07ff9eda10e64bea6b2f6bd7fae10d633af3afb8
Rolando Garcia <jrdosmil0@gmail.com>
2022-01-13 20:12:53 -0600

    New changes added

--- lib/test/test_chat_design.dart
+++ lib/test/test_chat_design.dart
@@ -1,12 +1,10 @@
 // Chat design:
 //https://dribbble.com/shots/8094338-Chat-App
 
-/*
- TODO: 
- 1.Create the bubble chat
- 2.Add the date next to the bubble
- */
 import 'package:flutter/material.dart';
+import 'package:videocalling_app/core/extensions/context_ext.dart';
+import 'package:videocalling_app/core/utils/utils.dart';
+import 'package:videocalling_app/core/widgets/mini_widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,15 +14,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Chat Components',
       home: Scaffold(
+        backgroundColor: Utils.textFormFIeldColor,
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          child: ChatBubble(
+            bottomRightRadius: 0,
           ),
         ),
       ),
+    );
+  }
+}
+
+const _radius = 32.0;
+
+/// The space to show a message in the chat
+class ChatBubble extends StatelessWidget {
+  const ChatBubble({
+    Key? key,
+    this.topLeftRadius = _radius,
+    this.bottomRightRadius = _radius,
+  })  : assert(topLeftRadius != bottomRightRadius),
+        super(key: key);
+
+  final double topLeftRadius, bottomRightRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = context.width * 0.6;
+
+    return Container(
+      width: width,
+      decoration: BoxDecoration(
+        // TODO: Add constraints to the chat bubble for large screens
+        // TODO: Add the date as the dessign suggest
+        color: Colors.purple,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(topLeftRadius),
+          bottomRight: Radius.circular(bottomRightRadius),
+          topRight: Radius.circular(_radius),
+          bottomLeft: Radius.circular(_radius),
+        ),
+      ),
+      padding: const EdgeInsets.all(18.0),
+      child: TextCustom(
+          'Hello There.... asdadsadsa asdasdasdasd sdsadasdsad asdsadsadasds'),
     );
   }
 }
